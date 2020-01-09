62bec4f0039e116062b7f08a5d8acc9c671f5b6d
yuutashiraishi <yuta.winner1@gmail.com>
2020-01-09 14:31:46 +0900

    PinBall　ゲーム作成
    PinBallゲームの作成をしたもの

---
+++ Assets/Score.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+    private GameObject scoreText;
+    int score = 0; //スコア計算用変数
+
+    void Start()
+    {
+        score = 0;
+        this.scoreText = GameObject.Find("scoreText");
+       
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        string yourTag = collision.gameObject.tag;
+
+        if (yourTag == "SmallCloudTag")
+        {
+            score += 10;
+        }
+        if (yourTag == "SmallStarTag")
+        {
+            score += 15;
+        }
+        if (yourTag == "LargeCloudTag")
+        {
+            score += 20;
+        }
+        else
+        {
+            score += 25;
+        }
+        this.scoreText.GetComponent<Text>().text = "Score:"+this.score.ToString();
+    }
+    
+
+    void Update()
+    {
+        
+
+    }
+}
